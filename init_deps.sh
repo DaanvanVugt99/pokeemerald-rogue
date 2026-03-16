79af437cc65d47c18ba8e9b799c9a5bb3b86b0f3
Daan van Vugt <64010229+DaanvanVugt99@users.noreply.github.com>
2026-03-16 21:38:17 +0100

    fix CI

--- init_deps.sh
+++ init_deps.sh
@@ -3,20 +3,20 @@
 echo "Gathering dependencies:"
 
 echo "Poryscript.."
-if [ -d "tools/poryscript" ] 
+if [ -d "tools/poryscript" ]
 then
   echo "Skipping (Already exists)"
 else
   mkdir -p tools/poryscript
-  wget -O tools/poryscript/poryscript-linux.zip https://github.com/huderlem/poryscript/releases/download/3.0.2/poryscript-linux.zip
-  wget -O tools/poryscript/poryscript-windows.zip https://github.com/huderlem/poryscript/releases/download/3.0.2/poryscript-windows.zip
-  unzip tools/poryscript/poryscript-linux.zip -d tools/poryscript
-  unzip tools/poryscript/poryscript-windows.zip -d tools/poryscript
+
+  wget -O tools/poryscript/poryscript-linux.zip https://github.com/huderlem/poryscript/releases/download/3.5.2/poryscript-linux.zip
+  unzip -o tools/poryscript/poryscript-linux.zip -d tools/poryscript
+
   echo "Done."
 fi
 
 echo "UPS Patcher.."
-if [ -d "tools/ups" ] 
+if [ -d "tools/ups" ]
 then
   echo "Skipping (Already exists)"
 else
