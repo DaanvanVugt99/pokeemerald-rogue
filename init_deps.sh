81bc44a4481a37699acde508ddf57cca6219feee
Daan van Vugt <64010229+DaanvanVugt99@users.noreply.github.com>
2026-03-16 21:45:06 +0100

    fix CI

--- init_deps.sh
+++ init_deps.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-set -e
-
-UNAME_S="$(uname -s)"
-
 echo "Gathering dependencies:"
 
 echo "Poryscript.."
@@ -12,9 +8,11 @@ then
   echo "Skipping (Already exists)"
 else
   mkdir -p tools/poryscript
-
   wget -O tools/poryscript/poryscript-linux.zip https://github.com/huderlem/poryscript/releases/download/3.5.2/poryscript-linux.zip
   unzip -o tools/poryscript/poryscript-linux.zip -d tools/poryscript
+
+  cp tools/poryscript/poryscript-linux/command_config.json tools/poryscript/command_config.json
+  cp tools/poryscript/poryscript-linux/font_config.json tools/poryscript/font_config.json
 
   echo "Done."
 fi
