e00969123af5456570b7931d6a2c35e59f101431
Daan van Vugt <64010229+DaanvanVugt99@users.noreply.github.com>
2026-04-14 13:52:18 +0200

    Fixed dynamo fists tests

--- test/battle/ability/unique/dynamo_fists.c
+++ test/battle/ability/unique/dynamo_fists.c
@@ -5,6 +5,7 @@ ASSUMPTIONS
 {
     ASSUME(gBattleMoves[MOVE_THUNDERBOLT].type == TYPE_ELECTRIC);
     ASSUME(gBattleMoves[MOVE_THUNDER_WAVE].type == TYPE_ELECTRIC);
+    ASSUME(gBattleMoves[MOVE_CHARGE].type == TYPE_ELECTRIC);
     ASSUME(gBattleMoves[MOVE_MEGA_PUNCH].punchingMove);
     ASSUME(!gBattleMoves[MOVE_TACKLE].punchingMove);
 }
@@ -12,14 +13,14 @@ ASSUMPTIONS
 SINGLE_BATTLE_TEST("Dynamo Fists gives +1 priority to the next punching move after an Electric move")
 {
     GIVEN {
-        PLAYER(SPECIES_ELECTABUZZ) { Speed(50); Ability(ABILITY_STATIC); UniqueAbility(ABILITY_DYNAMO_FISTS); Moves(MOVE_THUNDERBOLT, MOVE_MEGA_PUNCH); }
+        PLAYER(SPECIES_ELECTABUZZ) { Speed(50); Ability(ABILITY_STATIC); UniqueAbility(ABILITY_DYNAMO_FISTS); Moves(MOVE_ELECTRIC_TERRAIN, MOVE_MEGA_PUNCH); }
         OPPONENT(SPECIES_WOBBUFFET) { Speed(60); Moves(MOVE_CELEBRATE); }
     } WHEN {
-        TURN { MOVE(player, MOVE_THUNDERBOLT); MOVE(opponent, MOVE_CELEBRATE); }
+        TURN { MOVE(player, MOVE_ELECTRIC_TERRAIN); MOVE(opponent, MOVE_CELEBRATE); }
         TURN { MOVE(player, MOVE_MEGA_PUNCH); MOVE(opponent, MOVE_CELEBRATE); }
     } SCENE {
         ANIMATION(ANIM_TYPE_MOVE, MOVE_CELEBRATE, opponent);
-        ANIMATION(ANIM_TYPE_MOVE, MOVE_THUNDERBOLT, player);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_ELECTRIC_TERRAIN, player);
         ANIMATION(ANIM_TYPE_MOVE, MOVE_MEGA_PUNCH, player);
         ANIMATION(ANIM_TYPE_MOVE, MOVE_CELEBRATE, opponent);
     }
@@ -47,14 +48,15 @@ SINGLE_BATTLE_TEST("Dynamo Fists priority is consumed after the next punching mo
 SINGLE_BATTLE_TEST("Dynamo Fists does not consume priority on non-punching moves")
 {
     GIVEN {
-        PLAYER(SPECIES_ELECTABUZZ) { Speed(50); Ability(ABILITY_STATIC); UniqueAbility(ABILITY_DYNAMO_FISTS); Moves(MOVE_THUNDERBOLT, MOVE_TACKLE, MOVE_MEGA_PUNCH); }
+        PLAYER(SPECIES_ELECTABUZZ) { Speed(50); Ability(ABILITY_STATIC); UniqueAbility(ABILITY_DYNAMO_FISTS); Moves(MOVE_ELECTRIC_TERRAIN, MOVE_TACKLE, MOVE_MEGA_PUNCH); }
         OPPONENT(SPECIES_WOBBUFFET) { Speed(60); Moves(MOVE_CELEBRATE); }
     } WHEN {
-        TURN { MOVE(player, MOVE_THUNDERBOLT); MOVE(opponent, MOVE_CELEBRATE); }
+        TURN { MOVE(player, MOVE_ELECTRIC_TERRAIN); MOVE(opponent, MOVE_CELEBRATE); }
         TURN { MOVE(player, MOVE_TACKLE); MOVE(opponent, MOVE_CELEBRATE); }
         TURN { MOVE(player, MOVE_MEGA_PUNCH); MOVE(opponent, MOVE_CELEBRATE); }
     } SCENE {
         ANIMATION(ANIM_TYPE_MOVE, MOVE_CELEBRATE, opponent);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_ELECTRIC_TERRAIN, player);
         ANIMATION(ANIM_TYPE_MOVE, MOVE_TACKLE, player);
         ANIMATION(ANIM_TYPE_MOVE, MOVE_MEGA_PUNCH, player);
         ANIMATION(ANIM_TYPE_MOVE, MOVE_CELEBRATE, opponent);
@@ -68,10 +70,10 @@ SINGLE_BATTLE_TEST("Dynamo Fists boosts that punching move by 1.2x in Electric T
     PARAMETRIZE { setupMove = MOVE_ELECTRIC_TERRAIN; }
 
     GIVEN {
-        PLAYER(SPECIES_ELECTABUZZ) { Ability(ABILITY_STATIC); UniqueAbility(ABILITY_DYNAMO_FISTS); Moves(MOVE_THUNDER_WAVE, MOVE_MEGA_PUNCH); }
+        PLAYER(SPECIES_ELECTABUZZ) { Ability(ABILITY_STATIC); UniqueAbility(ABILITY_DYNAMO_FISTS); Moves(MOVE_CHARGE, MOVE_MEGA_PUNCH); }
         OPPONENT(SPECIES_WOBBUFFET) { Ability(ABILITY_BATTLE_ARMOR); Moves(setupMove, MOVE_CELEBRATE); }
     } WHEN {
-        TURN { MOVE(player, MOVE_THUNDER_WAVE); MOVE(opponent, setupMove); }
+        TURN { MOVE(player, MOVE_CHARGE); MOVE(opponent, setupMove); }
         TURN { MOVE(player, MOVE_MEGA_PUNCH); MOVE(opponent, MOVE_CELEBRATE); }
     } SCENE {
         HP_BAR(opponent, captureDamage: &results[i].punchDamage);
